a08928a64a663844eee436ff25bf973fccfb37ea
rich-bit <55553867+rich-bit@users.noreply.github.com>
2021-10-21 16:35:02 +0200

    testing C# string-split
    Going to split entries in stamp-clock.txt, then put in struct objects and at last present to user...

---
+++ TestFileRead/TestFileRead/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.IO;
+
+namespace TestFileRead
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // This will get the current WORKING directory (i.e. \bin\Debug)
+            string workingDirectory = Environment.CurrentDirectory;
+
+            // This will get the current PROJECT directory
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+            List<String> myStringList = File.ReadAllLines(projectDirectory + "/stamp-clock.txt").ToList();
+            foreach(string item in myStringList)
+            {
+                Console.WriteLine(item);
+                string[] subs = item.Split(new [] { "punch-in", ";" }, StringSplitOptions.RemoveEmptyEntries);//Gör lista av string[] splitters
+                Console.WriteLine(subs[0]);//Lägg i settings
+            }
+            Console.ReadLine();
+        }
+    }
+}
